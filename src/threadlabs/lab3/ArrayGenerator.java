68083dcd74f99a3daf4d4eb37947c2d314e66c14
Olga Kozlova <olgasergeevna0803@gmail.com>
2017-12-03 16:25:07 +0200

    Added XOR.

--- src/threadlabs/lab3/ArrayGenerator.java
+++ src/threadlabs/lab3/ArrayGenerator.java
@@ -1,17 +1,27 @@
 package threadlabs.lab3;
 
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
 public class ArrayGenerator {
     private static int[] array;
 
-    public static int[] generateArray(){
-        array = new int[100];
-        for(int i=0; i<100; i++){
+    public static int[] generateArray(int size){
+        array = new int[size];
+        for(int i=0; i<size; i++){
             if(i%2 == 0)
                 array[i] = 2;
             else array[i] = 1;
         }
-        array[5] = 100;
-        array[10] = 0;
+        array[size/2] = 100;
+        array[size - 1] = 0;
         return  array;
+    }
+
+    public static byte[] convertFromInt(int[] array){
+        ByteBuffer byteBuffer = ByteBuffer.allocate(array.length * 4);
+        IntBuffer intBuffer = byteBuffer.asIntBuffer();
+        intBuffer.put(array);
+        return byteBuffer.array();
     }
 }
--- src/threadlabs/lab3/Test.java
+++ src/threadlabs/lab3/Test.java
@@ -1,5 +1,7 @@
 package threadlabs.lab3;
 
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -9,8 +11,11 @@ public class Test {
         MyAtomicInteger count = new MyAtomicInteger();
         MyAtomicInteger max = new MyAtomicInteger();
         MyAtomicInteger min = new MyAtomicInteger();
+        MyAtomicInteger xor = new MyAtomicInteger();
 
-        int array[] = ArrayGenerator.generateArray();
+        int array[] = ArrayGenerator.generateArray(5);
+
+        byte[] byteArray = ArrayGenerator.convertFromInt(array);
 
         ExecutorService executor = Executors.newFixedThreadPool(3);
 
@@ -28,11 +33,18 @@ public class Test {
             }
         });
 
-        executor.shutdown();
-        executor.awaitTermination(2, TimeUnit.SECONDS);
+        executor.submit(()->{
+            for(byte i = 1; i<byteArray.length - 2; i++){
+                xor.set((byte) (xor.get()^byteArray[i]));
+            }
+        });
 
-        System.out.println(count.get());
-        System.out.println(max.get());
-        System.out.println(min.get());
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
+
+        System.out.println("Count - " + count.get());
+        System.out.println("Max - " + max.get());
+        System.out.println("Min - " + min.get());
+        System.out.println("XOR - " + xor.get());
     }
 }
